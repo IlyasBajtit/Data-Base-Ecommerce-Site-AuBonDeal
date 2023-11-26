1f2494944511d07f7095ed8003129a025baa934a
Ilyas <ilyas.bajtit@gmail.com>
2023-11-26 18:05:38 +0100

    docs: add products table

--- Data-Base.sql
+++ Data-Base.sql
@@ -5,3 +5,14 @@ CREATE TABLE users(
     user_password VARCHAR(50),
     created_at DATE NOT NULL
 );
+
+CREATE TABLE products (
+    product_uuid SERIAL PRIMARY KEY,
+    product_name VARCHAR(100),
+    product_description TEXT,
+    product_price DECIMAL (10,2) CHECK (product_price >= 0),
+    product_quantity INT CHECK (product_quantity >= 0),
+    created_at DATE NOT NULL,
+    updated_at DATE,
+    CONSTRAINT chk_dates CHECK (created_at < updated_at)
+); 
